ebf257007b19e095e59c3b9d5c0e4edbf97155db
Felipe Salazar Diaz <felipe.salazar.di@gmail.com>
2020-09-03 12:20:23 -0400

    Database

---
+++ app/src/main/java/cl/desafiolatam/herodataapp/model/database/HeroDatabase.kt
@@ -0,0 +1,35 @@
+package cl.desafiolatam.herodataapp.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [HeroEntity::class], version = 1)
+abstract class HeroDatabase : RoomDatabase() {
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: HeroDatabase? = null
+
+        fun getDatabase(context: Context): HeroDatabase {
+            val tempInstance =
+                INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HeroDatabase::class.java,
+                    "hero_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+}
