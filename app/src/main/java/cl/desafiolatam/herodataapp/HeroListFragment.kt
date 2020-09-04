5d7c561d664c83eb2b562c3f3246fb55bbcc7975
Felipe Salazar Diaz <felipe.salazar.di@gmail.com>
2020-09-04 10:56:21 -0400

    fragment recyclerView

---
+++ app/src/main/java/cl/desafiolatam/herodataapp/HeroListFragment.kt
@@ -0,0 +1,64 @@
+package cl.desafiolatam.herodataapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import cl.desafiolatam.herodataapp.model.pojo.HeroMini
+import cl.desafiolatam.herodataapp.view.HeroAdapter
+import cl.desafiolatam.herodataapp.viewmodel.HeroViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_hero_list.*
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class HeroListFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var heroList = ArrayList<HeroMini>()
+    private lateinit var adapter : HeroAdapter
+    private lateinit var heroViewModel : HeroViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hero_list, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                HeroListFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = HeroAdapter(heroList)
+        heroRecycler.adapter = adapter
+        //heroViewModel = ViewModelProvider(this).get(HeroViewModel::class.java)
+        val heroViewModel : HeroViewModel by activityViewModels()
+
+        heroViewModel.listHero.observe(viewLifecycleOwner, Observer {
+            adapter.updateItems(it)
+        })
+    }
+}
--- app/src/main/java/cl/desafiolatam/herodataapp/view/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/herodataapp/view/MainActivity.kt
@@ -16,22 +16,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var heroList = ArrayList<HeroMini>()
-    private lateinit var adapter : HeroAdapter
-    private lateinit var heroViewModel : HeroViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d("tag", "creando")
 
-        adapter = HeroAdapter(heroList)
-        heroRecycler.adapter = adapter
-        heroViewModel = ViewModelProvider(this).get(HeroViewModel::class.java)
-
-        heroViewModel.listHero.observe(this, Observer {
-            adapter.updateItems(it)
-        })
     }
 }
